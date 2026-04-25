4e6ca638e7b5d5b766d4c27a7187af9730c541f6
rthings <rickysatria442@gmail.com>
2026-04-25 14:55:16 +0700

    fix: explicit favicon link for better clarity

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'AsetKu Enterprise')</title>
+    <link rel="icon" type="image/png" href="{{ asset('logo.png') }}">
     <!-- Tailwind -->
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
